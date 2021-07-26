8ca4cf5ad4e4c1ba7313a8eba63b255ed0a47d0e
Lucas <lucasmirsoa@icloud.com>
2021-07-26 17:19:36 -0300

    Updates in the framework and Sample
    Updates to the main code and in the sample

--- EKeychainManager/EKeychainManager.swift
+++ EKeychainManager/EKeychainManager.swift
@@ -5,4 +5,82 @@
 //  Created by Lucas Soares on 26/07/21.
 //
 
-import Foundation
+import Security
+
+public final class EKeychainManager {
+    
+    private let keychainIdentifier: String
+    
+    public init(identifier: String) {
+        self.keychainIdentifier = identifier
+    }
+    
+    public func storeToken(with values: [String: Any], success: @escaping () -> Void, fail: @escaping () -> Void) {
+        
+        // Trying to add values
+        if SecItemAdd(values as CFDictionary, nil) == noErr {
+            success()
+        }
+        else { fail() }
+    }
+    
+    public func getStoredToken(success: @escaping(_ token: String) -> Void, fail: @escaping () -> Void) {
+        
+        var item: CFTypeRef?
+
+        // Check if content exists in the keychain
+        if SecItemCopyMatching(self.generateQuery() as CFDictionary, &item) == noErr {
+            // Extract the result
+            if let existingItem = item as? [String: Any],
+               let data = existingItem[kSecValueData as String] as? Data,
+               let token = String(data: data, encoding: .utf8) {
+                
+                success(token)
+            }
+            else { fail() }
+        }
+        else { fail() }
+    }
+    
+    public func setStoredToken(with values: [String: Any], success: @escaping () -> Void, fail: @escaping () -> Void) {
+
+        // Find values and update
+        if SecItemUpdate(self.generateQueryToUpdate() as CFDictionary, values as CFDictionary) == noErr {
+            success()
+        }
+        else { fail() }
+    }
+    
+    public func generateAttributes(with token: String) -> [String: Any] {
+        
+        return [ kSecClass as String: kSecClassGenericPassword,
+                 kSecAttrAccount as String: self.keychainIdentifier,
+                 kSecValueData as String: token.data(using: .utf8)! ]
+    }
+    
+    public func generateAttributesToUpdate(with token: String) -> [String: Any] {
+        
+        return [ kSecValueData as String: token.data(using: .utf8)! ]
+    }
+}
+
+
+// MARK: - Private methods
+
+extension EKeychainManager {
+    
+    private func generateQuery() -> [String: Any] {
+        
+        return [ kSecClass as String: kSecClassGenericPassword,
+                 kSecAttrAccount as String: self.keychainIdentifier,
+                 kSecMatchLimit as String: kSecMatchLimitOne,
+                 kSecReturnAttributes as String: true,
+                 kSecReturnData as String: true ]
+    }
+    
+    private func generateQueryToUpdate() -> [String: Any] {
+        
+        return [ kSecClass as String: kSecClassGenericPassword,
+                 kSecAttrAccount as String: self.keychainIdentifier ]
+    }
+}
--- EKeychainManagerSample/ViewController.swift
+++ EKeychainManagerSample/ViewController.swift
@@ -6,21 +6,58 @@
 //
 
 import UIKit
-import Security
+import EKeychainManager
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var UserTextField: UITextField!
-    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var TokenTextField: UITextField!
     
+    private var keychainManager: EKeychainManager?
     private let keychainIdentifier = "identifier_\(UIDevice.current.identifierForVendor!.uuidString)"
     
-    @IBAction func StoreButtonTapped(_ sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-//        if self.tryToGetStoredValuesFromKeychain() {
-//            debugPrint("Valores encontrados no keychain")
-//        }
-//        else { self.tryToStoreValuesInKeychain() }
+        self.keychainManager = EKeychainManager(identifier: self.keychainIdentifier)
+    }
+    
+    @IBAction func StoreButtonTapped(_ sender: UIButton) {
+        self.checkIfTokenAlreadyExists()
+    }
+    
+    private func checkIfTokenAlreadyExists() {
+        
+        self.keychainManager?.getStoredToken(success: {
+            token in
+            print("token already exist: (stored token) \(token)")
+            print("let's try to set to the new value")
+            self.setTheStoredTokenValue()
+            
+        }, fail: {
+            self.storeTokenInKeychain()
+        })
+    }
+    
+    private func storeTokenInKeychain() {
+        guard let token = TokenTextField.text else { return }
+        guard let keychainAttributes = self.keychainManager?.generateAttributes(with: token) else { return }
+        
+        self.keychainManager?.storeToken(with: keychainAttributes, success: {
+            print("token stored with success - \(token)")
+        }, fail: {
+            print("fail to storage the token")
+        })
+    }
+    
+    private func setTheStoredTokenValue() {
+        guard let token = TokenTextField.text else { return }
+        guard let keychainAttributes = self.keychainManager?.generateAttributesToUpdate(with: token) else { return }
+        
+        self.keychainManager?.setStoredToken(with: keychainAttributes, success: {
+            print("token updated with success - \(token)")
+        }, fail: {
+            print("fail to update the token")
+        })
     }
 }
 
