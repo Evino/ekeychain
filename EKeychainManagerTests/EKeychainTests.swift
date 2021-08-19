b5e6385de1730ba4f3b62def2879f18758431ab7
Lucas <lucasmirsoa@icloud.com>
2021-08-19 10:59:21 -0300

    Tests updated

--- EKeychainManagerTests/EKeychainTests.swift
+++ EKeychainManagerTests/EKeychainTests.swift
@@ -13,29 +13,66 @@ class EKeychainTests: XCTestCase {
     let keychainManager = EKeychainManager(identifier: "EKeychainTests_identifier")
     
     func testGetStoredTokenSuccess() {
-        testStoreTokenSuccess()
-//        XCTAssertNotNil(keychainManager.getStoredAttribute())
+                
+        wait(seconds: 1) {
+            self.testStoreTokenSuccess()
+            XCTAssertNotNil(self.keychainManager.getStoredAttribute())
+        }
     }
     
     func testStoreTokenSuccess() {
         
-        keychainManager.storeAttribute(with: "TOKEN_123!@#-STORED", success: {
-            XCTAssert(true)
-        }, fail: { })
+        wait(seconds: 1) {
+            self.keychainManager.storeAttribute(with: "TOKEN_123!@#-STORED", success: {
+                XCTAssert(true)
+            }, fail: {
+                XCTAssert(false)
+            })
+        }
     }
     
     func testUpdateTokenSuccess() {
         
-        keychainManager.setStoredAttribute(with: "TOKEN_123!@#-UPDATED", success: {
-            XCTAssert(true)
-        }, fail: { })
+        wait(seconds: 1) {
+            self.keychainManager.setStoredAttribute(with: "TOKEN_123!@#-UPDATED", success: {
+                XCTAssert(true)
+            }, fail: {
+                XCTAssert(false)
+            })
+        }
     }
     
     func testDeleteTokenSuccess() {
         
-        keychainManager.deleteStoredAttribute(success: {
-            XCTAssert(true)
-        }, fail: { })
+        wait(seconds: 1) {
+            self.keychainManager.deleteStoredAttribute(success: {
+                XCTAssert(true)
+            }, fail: {
+                XCTAssert(false)
+            })
+        }
     }
 
+}
+
+
+private extension EKeychainTests {
+    
+    func wait(seconds: Double, completion: @escaping () -> Void) {
+            
+        let time = DispatchTime.now() + seconds
+        
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            
+            completion()
+        }
+    }
+    
+    func now(completion: @escaping () -> Void) {
+    
+        DispatchQueue.main.async {
+            
+            completion()
+        }
+    }
 }
